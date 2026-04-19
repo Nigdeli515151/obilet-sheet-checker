6d7699bcfeec704a8640155b6599383c2f0cb86e
Nigdeli515151 <1kemalsenel@gmail.com>
2026-04-20 00:19:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -31,11 +31,16 @@ const USER_AGENTS = [
 ];
 
 function getTomorrowDateTR() {
-  const now = new Date();
-  now.setDate(now.getDate() + 1);
-  const y = now.getFullYear();
-  const m = String(now.getMonth() + 1).padStart(2, "0");
-  const d = String(now.getDate()).padStart(2, "0");
+  const trNow = new Date(
+    new Date().toLocaleString("en-US", { timeZone: "Europe/Istanbul" })
+  );
+
+  trNow.setDate(trNow.getDate() + 1);
+
+  const y = trNow.getFullYear();
+  const m = String(trNow.getMonth() + 1).padStart(2, "0");
+  const d = String(trNow.getDate()).padStart(2, "0");
+
   return `${y}-${m}-${d}`;
 }
 
